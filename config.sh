92b442175e4a9480eb5c0a4f35554c1536e83649
Murray Wham <murray.wham@ed.ac.uk>
2015-06-29 14:15:48 +0100

    #1: Adding additional stdout

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 ### Configuration ###
 
 # User-configurable file paths, exectuables, etc.
-EXECROOT=$HOME/bin  # Path to executables. Set up symbolic links to the Process Trigger bash scripts
-WORKFLOWEXE=$EXECROOT/ptworkflow
-TTAGENTEXE=$EXECROOT/ttagent
+EXECROOT=$HOME/Process-Trigger  # Path to executables. Set up symbolic links to the Process Trigger bash scripts
+WORKFLOWEXE=$EXECROOT/ptworkflow.sh
+TTAGENTEXE=$EXECROOT/ttagent.sh
 TTDELAY=120
 
 DATAROOT=$HOME/raw  #/sequencer/RAW  # Path to rdf mount 
@@ -15,6 +15,6 @@ DATAREGEXP='^.*/[0-9]{6}_E[0-9]{5}_.*_.*$'
 TRIGGER=RTAComplete.txt
 AGEINMINS=5
 
-PYTHON=$HOME/envs/23_06_15/bin/python
+PYTHON=$HOME/python/envs/23_06_15/bin/python
 ANALYSISDRIVER=$HOME/Analysis-Driver
 
--- ptworkflow.sh
+++ ptworkflow.sh
@@ -3,7 +3,7 @@ source config.sh
 
 echo "[ptworkflow] Processing workflow for: $1"
 
-# Run the driver that kicks off all the processing jobs
 cd $ANALYSISDRIVER
-$PYTHON driver.py $1
+# $PYTHON 
+python driver.py $1  # Run the driver that kicks off all the processing jobs
 
